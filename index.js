bbd10ef9f0d9bdfbd52a1562d7e4fac3ba545977
Yosef G <withstunna005@gmail.com>
2024-09-23 16:24:35 +0000

    Do req header parser microservice

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res)=> {
+  const clientip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const language = req.headers["accept-language"]
+  const software = req.headers["user-agent"]
+  res.json({
+    ipaddress: clientip,
+    language,
+    software
+
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
